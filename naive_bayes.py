3b6b7cc3a16b5762d74a4660eada1a53f0514e7a
Till Bungert <tillbungert@gmail.com>
2017-11-07 11:57:31 +0100

    Add basic code skeleton

---
+++ naive_bayes.py
@@ -0,0 +1,14 @@
+import pytorch.nn as nn
+
+
+class NaiveBayes(nn.Module):
+    """ Implementation of Naive Bayes as a layer for pytorch models
+
+    TODO
+    
+    """
+    def __init__(self, features, classes):
+        super(self.__class__, self).__init__()
+
+    def forward(self, x):
+        pass
