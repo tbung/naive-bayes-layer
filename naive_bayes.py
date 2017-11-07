740adc45bcfafabefbd65f0035abba195dc40bac
Till Bungert <tillbungert@gmail.com>
2017-11-07 15:35:42 +0100

    First implementation attempt
    Still needs parameter initialization

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,14 +1,37 @@
-import pytorch.nn as nn
+import torch
+import torch.nn as nn
+
+from numpy import pi
 
 
-class NaiveBayes(nn.Module):
+class GaussianNaiveBayes(nn.Module):
     """ Implementation of Naive Bayes as a layer for pytorch models
 
     TODO
+    ----
+    - Make std devs fixable
     
     """
     def __init__(self, features, classes):
         super(self.__class__, self).__init__()
 
+        # We need mean and variance per feature and class
+        self.register_parameter(
+            "means",
+            nn.Parameter(torch.Tensor(self.classes, self.features))
+        )
+        self.register_parameter(
+            "variances",
+            nn.Parameter(torch.Tensor(self.classes, self.features))
+        )
+
+        # We need the class priors
+        self.register_parameter(
+            "log_class_priors",
+            nn.Parameter(torch.Tensor(self.classes))
+        )
+
     def forward(self, x):
-        pass
+        return (self.log_class_priors
+                + torch.sum(- 0.5 * torch.log(2 * pi * self.variances), axis=1)
+                + torch.sum((x - self.means)**2 / self.variances, axis=1))
