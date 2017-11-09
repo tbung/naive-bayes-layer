38ce7f5892d1bf1dbca3132a4089392fe2ccd16a
Till Bungert <tillbungert@gmail.com>
2017-11-09 13:08:27 +0100

    Fix typo and make variances fixable

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
 from numpy import pi
 import numpy as np
@@ -13,7 +14,7 @@ class GaussianNaiveBayes(nn.Module):
     - Make std devs fixable
     - Look into better param initialization
     """
-    def __init__(self, features, classes):
+    def __init__(self, features, classes, fix_variance=True):
         super(self.__class__, self).__init__()
 
         self.features = features
@@ -24,10 +25,16 @@ class GaussianNaiveBayes(nn.Module):
             "means",
             nn.Parameter(torch.Tensor(self.classes, self.features))
         )
-        self.register_parameter(
-            "variances",
-            nn.Parameter(torch.Tensor(self.classes, self.features))
-        )
+        if not fix_variance:
+            self.register_parameter(
+                "variances",
+                nn.Parameter(torch.Tensor(self.classes, self.features))
+            )
+        else:
+            self.register_buffer(
+                "variances",
+                Variable(torch.Tensor(self.classes, self.features))
+            )
 
         # We need the class priors
         self.register_parameter(
@@ -38,12 +45,12 @@ class GaussianNaiveBayes(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.means.data.zero_()
+        self.means.data.normal_()
         self.variances.data.fill_(1)
         self.class_priors.data.fill_(1/self.classes)
 
     def forward(self, x):
         x = x[:,np.newaxis,:]
         return (torch.log(self.class_priors)
-                + torch.sum(- 0.5 * torch.log(2 * pi * self.variances), dim=1)
-                - torch.sum((x - self.means)**2 / self.variances, dim=1))
+                + torch.sum(- 0.5 * torch.log(2 * pi * torch.abs(self.variances)), dim=1)
+                - torch.sum((x - self.means)**2 / torch.abs(self.variances) / 2, dim=1))
